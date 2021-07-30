814ed092746fa1b7cec98d3a2ab640b1651a936c
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-07-30 16:03:36 -0400

    Initial Commit

---
+++ CVBuilder/CVBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVBuilderApp.swift
+//  CVBuilder
+//
+//  Created by Djallil Elkebir on 2021-07-30.
+//
+
+import SwiftUI
+
+@main
+struct CVBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
